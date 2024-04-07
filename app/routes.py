ae3aeb10b63d07e24efabcfbf113df09cafdb418
livvy10 <olivia_hanly@icloud.com>
2024-04-07 16:49:04 +0800

    Create base.html for nav bar and add basic html for profilePage.html

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,12 @@
 from flask import render_template
 from app import app
 @app.route('/')
-@app.route('/index')
-def index():
-    render_template("profilePage.html")
+@app.route('/profile')
+def profile():
+    user={ 'username' : 'student1',
+            'email' : 'student1@uwa.com',
+            'position' : 'Undergraduate Student',
+            'study' : ['Computer Science', 'French'],
+            'bio' : 'Attending UWA, studying a Bachelor of Science majoring in CompSci and a minor in French. No previous programming experience. Hobbies include hiking and knitting.'
+          }
+    return render_template("profilePage.html", user=user)
