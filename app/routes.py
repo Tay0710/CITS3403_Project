74bf5119fcb99902c8f846ae414a81510b6ec23e
livvy10 <olivia_hanly@icloud.com>
2024-04-07 19:04:57 +0800

    Move profile edit button to bottom and create createProfile form page

--- app/routes.py
+++ app/routes.py
@@ -3,10 +3,16 @@ from app import app
 @app.route('/')
 @app.route('/profile')
 def profile():
-    user={ 'username' : 'student1',
+    validatedUser=True
+    user={  'fname' : 'John',
+            'lname' : 'Smith',
             'email' : 'student1@uwa.com',
             'position' : 'Undergraduate Student',
             'study' : ['Computer Science', 'French'],
             'bio' : 'Attending UWA, studying a Bachelor of Science majoring in CompSci and a minor in French. No previous programming experience. Hobbies include hiking and knitting.'
           }
-    return render_template("profilePage.html", user=user)
+    return render_template("profilePage.html", user=user, validatedUser=validatedUser)
+
+@app.route('/createProfile')
+def createProfile():
+    return render_template("createProfile.html")
