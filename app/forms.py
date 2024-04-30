afc341af41b47aa667be3530890701cda57c3389
livvy10 <olivia_hanly@icloud.com>
2024-05-01 01:51:28 +0800

    Add login and create profile functionality

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,105 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, RadioField, SelectField, TextAreaField
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+import sqlalchemy as sa
+from app import db
+from app.models import User
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Log In')
+
+class CreateProfileForm(FlaskForm):
+    fname = StringField('First Name', validators=[DataRequired()])
+    lname = StringField('Last Name', validators=[DataRequired()])
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    position = RadioField('Position', choices=[
+        ('Undergraduate Student', 'Undergraduate Student'),
+        ('Postgraduate Student', 'Postgraduate Student'),
+        ('Staff', 'Staff'),
+        ('Future Student', 'Future Student'),
+        ('Graduate', 'Graduate')
+    ], validators=[DataRequired()])
+    study = SelectField('Area of Study', choices=[
+        ('Accounting', 'Accounting'),
+        ('Agricultural Science', 'Agricultural Science'),
+        ('Anatomy and Human Biology', 'Anatomy and Human Biology'),
+        ('Anthropology', 'Anthropology'),
+        ('Architecture', 'Architecture'),
+        ('Asian Studies', 'Asian Studies'),
+        ('Biochemistry', 'Biochemistry'),
+        ('Botany', 'Botany'),
+        ('Business Law', 'Business Law'),
+        ('Chemical Engineering', 'Chemical Engineering'),
+        ('Chemistry', 'Chemistry'),
+        ('Civil Engineering', 'Civil Engineering'),
+        ('Classical Studies', 'Classical Studies'),
+        ('Computer Science', 'Computer Science'),
+        ('Conservation Biology', 'Conservation Biology'),
+        ('Data Science', 'Data Science'),
+        ('Dentistry', 'Dentistry'),
+        ('Earth Sciences', 'Earth Sciences'),
+        ('Economics', 'Economics'),
+        ('Education', 'Education'),
+        ('Electrical Engineering', 'Electrical Engineering'),
+        ('Environmental Engineering', 'Environmental Engineering'),
+        ('Environmental Science', 'Environmental Science'),
+        ('Finance', 'Finance'),
+        ('French Studies', 'French Studies'),
+        ('Genetics', 'Genetics'),
+        ('Geography', 'Geography'),
+        ('Geology', 'Geology'),
+        ('History', 'History'),
+        ('Human Resource Management', 'Human Resource Management'),
+        ('Indigenous Knowledge, History, and Heritage', 'Indigenous Knowledge, History, and Heritage'),
+        ('Indonesian Studies', 'Indonesian Studies'),
+        ('Information Technology', 'Information Technology'),
+        ('Japanese Studies', 'Japanese Studies'),
+        ('Law', 'Law'),
+        ('Linguistics', 'Linguistics'),
+        ('Management', 'Management'),
+        ('Marine Science', 'Marine Science'),
+        ('Marketing', 'Marketing'),
+        ('Mathematics', 'Mathematics'),
+        ('Mechanical Engineering', 'Mechanical Engineering'),
+        ('Medicine', 'Medicine'),
+        ('Microbiology', 'Microbiology'),
+        ('Music', 'Music'),
+        ('Neuroscience', 'Neuroscience'),
+        ('Nursing', 'Nursing'),
+        ('Petroleum Engineering', 'Petroleum Engineering'),
+        ('Philosophy', 'Philosophy'),
+        ('Physics', 'Physics'),
+        ('Physiology', 'Physiology'),
+        ('Political Science and International Relations', 'Political Science and International Relations'),
+        ('Psychology', 'Psychology'),
+        ('Public Health', 'Public Health'),
+        ('Renewable Energy Engineering', 'Renewable Energy Engineering'),
+        ('Social Work', 'Social Work'),
+        ('Sociology', 'Sociology'),
+        ('Spanish Studies', 'Spanish Studies'),
+        ('Sport Science', 'Sport Science'),
+        ('Statistics', 'Statistics'),
+        ('Sustainable Development', 'Sustainable Development'),
+        ('Theatre Studies', 'Theatre Studies'),
+        ('Urban and Regional Planning', 'Urban and Regional Planning'),
+        ('Zoology', 'Zoology')
+    ], validators=[DataRequired()])
+    bio = TextAreaField('Bio', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Create Profile')
+
+    def validate_username(self, username):
+        user = db.session.scalar(sa.select(User).where(User.username == username.data))
+        if user is not None:
+            raise ValidationError('This username is already taken.')    
+        
+    def validate_email(self, email):
+        user = db.session.scalar(sa.select(User).where(
+            User.email == email.data))
+        if user is not None:
+            raise ValidationError('This email is already used.')    
--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,31 @@
 from flask import render_template, request, url_for, redirect, flash
+from urllib.parse import urlsplit
 import sqlite3
-
 from app import app
 from app.forms import LoginForm
+from flask_login import current_user, login_user
+import sqlalchemy as sa
+from app import db
+from app.models import User
+from flask_login import logout_user
+from flask_login import login_required
+from app.forms import CreateProfileForm
+
 
 @app.route('/', methods=['GET','POST'])
 def home():
+    #if current_user.is_authenticated:
+        #return redirect(url_for('forum'))
     form = LoginForm()
     if form.validate_on_submit():
-        return redirect((url_for('forum')))
+        user = db.session.scalar(sa.select(User).where(User.username == form.username.data))
+        if user is None or not user.check_password(form.password.data):
+            return redirect((url_for('home')))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or urlsplit(next_page).netloc != '':
+            next_page = url_for('forum')
+        return redirect(next_page)
     return render_template('home.html', title='Home', form=form)
 
 @app.route('/submit', methods=['POST'])
@@ -32,6 +49,7 @@ def thank_you():
     return render_template('thank_you.html')
 
 @app.route('/profile')
+@login_required
 def profile():
     validatedUser=True
     user={  'fname' : 'John',
@@ -44,14 +62,30 @@ def profile():
     return render_template("profile.html", user=user, validatedUser=validatedUser, title='Profile')
 
 @app.route('/forum')
+@login_required
 def forum():
     return render_template("forum.html", title='Forum')
 
 @app.route('/post')
+@login_required
 def post():
     return render_template("post.html", title='Post')
 
-@app.route('/createProfile')
+@app.route('/createProfile', methods=['GET', 'POST'])
 def createProfile():
-    return render_template("createProfile.html", title='Create Profile')
+    form = CreateProfileForm()
+    if form.validate_on_submit():
+        user = User(fname=form.fname.data, lname=form.lname.data, username=form.username.data, email=form.email.data,
+                    position=form.position.data, study=form.study.data, bio=form.bio.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template("createProfile.html", title='Create Profile', form=form)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
     
