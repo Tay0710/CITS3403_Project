450b3eae213a873856bff1bff359062c317c9bfc
TayTay0710 <taylah0710@gmail.com>
2024-05-16 12:33:22 +0800

    Updated to include test connection.

--- studiVault.py
+++ studiVault.py
@@ -1,7 +1,13 @@
 import sqlalchemy as sa
 import sqlalchemy.orm as so
-from app import app, db
+from app import db, create_app
 from app.models import User, Questions, Comments
+
+from flask_migrate import Migrate
+from config import DeploymentConfig
+
+app = create_app(DeploymentConfig)
+migrate = Migrate(app, db)
 
 @app.shell_context_processor
 def make_shell_context():
