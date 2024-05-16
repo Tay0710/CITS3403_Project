3b31a76895a57342412a3f064cd79f02710eee53
TayTay0710 <taylah0710@gmail.com>
2024-05-16 12:33:54 +0800

    Updated to include test creation.

--- app/__init__.py
+++ app/__init__.py
@@ -4,15 +4,19 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 
-
-app = Flask(__name__)
-
-app.config.from_object(Config)
-
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-
-login = LoginManager(app)
+db = SQLAlchemy()
+login = LoginManager()
 login.login_view = 'home'
+
+def create_app(Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    from app.blueprints import main
+    app.register_blueprint(main)
+    db.init_app(app)
+    login.init_app(app)
+    
+    return app
 
 from app import routes, models
