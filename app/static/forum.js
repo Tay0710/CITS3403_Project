bd88883ffa3f31300130b645b9799ca98315f537
TayTay0710 <taylah0710@gmail.com>
2024-05-18 18:33:05 +0800

    Remove unnecessary comments and spaces, and add section comments.

--- app/static/forum.js
+++ app/static/forum.js
@@ -1,17 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
     var topicSelect = document.getElementById("topics");
     var subtopicSelect = document.getElementById("subtopic");
-    var subForumContainer = document.querySelector('.subForum')
+    var subForumContainer = document.querySelector('.subForum');
     var subtopics = {
         "General University Questions": ["Accommodation", "Campus Events", "Health and Wellness", "Internships", "Scholarships", "Student Clubs", "Technology and Resources", "Transportation and Parking"],
         "University Courses": ["Mathematics", "Physics", "Literature", "Engineering", "ComputerScience", "Business"],
     };
 
-    // Disable the subtopic select by default
-    subtopicSelect.disabled = true; 
-        
+    subtopicSelect.disabled = true;
 
-    topicSelect.addEventListener("change", function() {     // Function to update subtopic dropdown menu based on the selected topic  
+    topicSelect.addEventListener("change", function() {
         var selectedTopic = topicSelect.value;
         var selectedSubtopic = subtopicSelect.value;
 
@@ -26,8 +24,7 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 
-
-    subtopicSelect.addEventListener("change", function() {          // Pick subfilter based on topic filter selected
+    subtopicSelect.addEventListener("change", function() {
         var selectedSubtopic = subtopicSelect.value;
         var selectedTopic = topicSelect.value;
         if (selectedSubtopic !== "") {
@@ -38,19 +35,18 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 
-    
-    function dropDownOptions(selectedTopic) {           // Function about topic to subtopic filter behaviours
-        subtopicSelect.innerHTML = "";      // Clear existing options
+    function dropDownOptions(selectedTopic) {
+        subtopicSelect.innerHTML = "";
         subtopicSelect.disabled = true;
 
-        if (selectedTopic !== "" && subtopics[selectedTopic]) {        // If a valid topic is selected, populate subtopic options
+        if (selectedTopic !== "" && subtopics[selectedTopic]) {
             subtopicSelect.disabled = false;
 
-            var defaultOption = document.createElement("option");       // Add default option
+            var defaultOption = document.createElement("option");
             defaultOption.text = "All Subtopics";
-            subtopicSelect.add(defaultOption)
-        
-            subtopics[selectedTopic].forEach(function(subtopic) {       // Add subtopic options based on the selected topic
+            subtopicSelect.add(defaultOption);
+
+            subtopics[selectedTopic].forEach(function(subtopic) {
                 var option = document.createElement("option");
                 option.text = subtopic;
                 subtopicSelect.add(option);
@@ -58,14 +54,13 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     }
 
-
-    function showAllPosts() {               // Function to show all posts without grouping 
-        var allPosts = document.querySelectorAll('.subForumRow');           // Remove 'hidden' css to reveal all posts
-            allPosts.forEach(function(post) {
-                post.classList.remove('hidden');
+    function showAllPosts() {
+        var allPosts = document.querySelectorAll('.subForumRow');
+        allPosts.forEach(function(post) {
+            post.classList.remove('hidden');
         });
 
-        if (topicSelect.value !== "All Topics") {           // Apply 'hidden' to all posts that are not the selected topic 
+        if (topicSelect.value !== "All Topics") {
             var groupedPosts = document.querySelectorAll('.subForumRow[data-topic]');
             groupedPosts.forEach(function(post) {
                 var topic = post.dataset.topic;
@@ -76,29 +71,27 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     }
 
-
-    function PostsByFilter(selectedTopic, selectedSubtopic) {            // Function to show/hide posts based on the selected topic
+    function PostsByFilter(selectedTopic, selectedSubtopic) {
         var allPosts = document.querySelectorAll('.subForumRow');
         allPosts.forEach(function(post) {
             if (selectedTopic !== "" && selectedTopic !== "All Topics" && post.dataset.topic !== selectedTopic) {
                 post.classList.add('hidden');
-                } else {
+            } else {
                 post.classList.remove('hidden');
             }
         });
-        updateTitle(selectedTopic, selectedSubtopic);       //Display the correct Title for selected Topic
+        updateTitle(selectedTopic, selectedSubtopic);
     }
 
-
-    function updateTitle(selectedTopic, selectedSubtopic) {     //Function to apply correct title additions based on subtopic
+    function updateTitle(selectedTopic, selectedSubtopic) {
         var titleText;
         if (selectedSubtopic && selectedSubtopic !== "All Subtopics") {
             titleText = selectedTopic + " - " + selectedSubtopic;
         } else {
-            titleText = selectedTopic
+            titleText = selectedTopic;
         }
 
-        var titleExists = document.querySelector('.subForumTitle');         
+        var titleExists = document.querySelector('.subForumTitle');
 
         if (!titleExists) {
             var subForumTitle = document.createElement('div');
@@ -110,14 +103,14 @@ document.addEventListener("DOMContentLoaded", function() {
         } else {
             titleExists.querySelector('h2').textContent = titleText;
         }
-    }    
-    updateTitle(topicSelect.value, subtopicSelect.value)           //Show the title initially 
+    }
 
+    updateTitle(topicSelect.value, subtopicSelect.value);
 
-    function filterBySubtopic(selectedSubtopic) {           // Filter posts by subtopic 
+    function filterBySubtopic(selectedSubtopic) {
         var selectedTopic = topicSelect.value;
 
-        if (selectedSubtopic == "All Subtopics") { 
+        if (selectedSubtopic == "All Subtopics") {
             PostsByFilter(selectedTopic);
         } else {
             var allPosts = document.querySelectorAll('.subForumRow');
@@ -127,11 +120,10 @@ document.addEventListener("DOMContentLoaded", function() {
                 } else {
                     post.classList.remove('hidden');
                 }
-            }); 
+            });
         }
     }
 
-    // No formatting but comment and post showing
     var searchInput = document.querySelector('.search-bar input');
     var searchButton = document.querySelector('.search-bar button');
 
@@ -140,102 +132,95 @@ document.addEventListener("DOMContentLoaded", function() {
         if (searchText !== '') {
             search(searchText);
         } else {
-            window.location.href = '/forum'; // Link to the original forum page
+            window.location.href = '/forum';
         }
     });
 
-// Add this function to handle the search functionality for both posts and comments
-function search(searchText) {
-    fetch('/search', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ searchTerm: searchText })
-    })
-    .then(response => response.json())
-    .then(data => {
-        displaySearchResults(data.posts, data.comments);
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
-}
+    function search(searchText) {
+        fetch('/search', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ searchTerm: searchText })
+        })
+        .then(response => response.json())
+        .then(data => {
+            displaySearchResults(data.posts, data.comments);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+    }
 
-function displaySearchResults(postResults, commentResults) {
-    var subForumContainer = document.querySelector('.subForum');
-    subForumContainer.innerHTML = '';
+    function displaySearchResults(postResults, commentResults) {
+        var subForumContainer = document.querySelector('.subForum');
+        subForumContainer.innerHTML = '';
 
-    // Display posts header
-    var postHeader = document.createElement('h2');
-    postHeader.textContent = 'Matching Posts';
-    postHeader.classList.add('section-header'); // Add class for styling
-    subForumContainer.appendChild(postHeader);
+        var postHeader = document.createElement('h2');
+        postHeader.textContent = 'Matching Posts';
+        postHeader.classList.add('section-header');
+        subForumContainer.appendChild(postHeader);
 
-    // Display post results
-    postResults.forEach(result => {
-        var postRow = document.createElement('div');
-        postRow.classList.add('subForumRow');
-        postRow.dataset.postId = result.post_id;
+        postResults.forEach(result => {
+            var postRow = document.createElement('div');
+            postRow.classList.add('subForumRow');
+            postRow.dataset.postId = result.post_id;
 
-        var postContent = document.createElement('div');
-        postContent.classList.add('post');
+            var postContent = document.createElement('div');
+            postContent.classList.add('post');
 
-        var postLink = document.createElement('a');
-        postLink.classList.add('post_title');
-        postLink.href = `/post/${result.post_id}`;
-        postLink.textContent = result.title;
-        postContent.appendChild(postLink);
+            var postLink = document.createElement('a');
+            postLink.classList.add('post_title');
+            postLink.href = `/post/${result.post_id}`;
+            postLink.textContent = result.title;
+            postContent.appendChild(postLink);
 
-        var postInfo = document.createElement('div');
-        postInfo.classList.add('post_info');
-        postInfo.innerHTML = `
-            <p>by <a href="/profile/${result.username}" class="boldInfoLink">${result.username}</a> (${moment(result.timestamp).format('LLL')})</p>
-            <p class="descriptionText">${result.description}</p>
-        `;
-        postContent.appendChild(postInfo);
+            var postInfo = document.createElement('div');
+            postInfo.classList.add('post_info');
+            postInfo.innerHTML = `
+                <p>by <a href="/profile/${result.username}" class="boldInfoLink">${result.username}</a> (${moment(result.timestamp).format('LLL')})</p>
+                <p class="descriptionText">${result.description}</p>
+            `;
+            postContent.appendChild(postInfo);
 
-        postRow.appendChild(postContent);
-        subForumContainer.appendChild(postRow);
-    });
+            postRow.appendChild(postContent);
+            subForumContainer.appendChild(postRow);
+        });
 
-    // Display comments header
-    var commentHeader = document.createElement('h2');
-    commentHeader.textContent = 'Matching Comments';
-    commentHeader.classList.add('section-header'); // Add class for styling
-    subForumContainer.appendChild(commentHeader);
+        var commentHeader = document.createElement('h2');
+        commentHeader.textContent = 'Matching Comments';
+        commentHeader.classList.add('section-header');
+        subForumContainer.appendChild(commentHeader);
 
-    // Display comments
-    commentResults.forEach(result => {
-        var commentRow = document.createElement('div');
-        commentRow.classList.add('subForumRow');
-        commentRow.dataset.postId = result.post_id;
+        commentResults.forEach(result => {
+            var commentRow = document.createElement('div');
+            commentRow.classList.add('subForumRow');
+            commentRow.dataset.postId = result.post_id;
 
-        var commentContent = document.createElement('div');
-        commentContent.classList.add('post');
+            var commentContent = document.createElement('div');
+            commentContent.classList.add('post');
 
-        var commentLink = document.createElement('a');
-        commentLink.classList.add('post_title');
-        commentLink.href = `/post/${result.post_id}`;
-        commentLink.textContent = result.post_title;
-        commentContent.appendChild(commentLink);
+            var commentLink = document.createElement('a');
+            commentLink.classList.add('post_title');
+            commentLink.href = `/post/${result.post_id}`;
+            commentLink.textContent = result.post_title;
+            commentContent.appendChild(commentLink);
 
-        var commentInfo = document.createElement('div');
-        commentInfo.classList.add('post_info');
-        commentInfo.innerHTML = `
-            <p>by <a href="/profile/${result.username}" class="boldInfoLink">${result.username}</a> (${moment(result.timestamp).format('LLL')})</p>
-            <p><strong>Matching Comment:</strong> ${result.comment_text}</p>
-        `;
-        commentContent.appendChild(commentInfo);
+            var commentInfo = document.createElement('div');
+            commentInfo.classList.add('post_info');
+            commentInfo.innerHTML = `
+                <p>by <a href="/profile/${result.username}" class="boldInfoLink">${result.username}</a> (${moment(result.timestamp).format('LLL')})</p>
+                <p><strong>Matching Comment:</strong> ${result.comment_text}</p>
+            `;
+            commentContent.appendChild(commentInfo);
 
-        commentRow.appendChild(commentContent);
-        subForumContainer.appendChild(commentRow);
-    });
-
-    
-}
-
+            commentRow.appendChild(commentContent);
+            subForumContainer.appendChild(commentRow);
+        });
+    }
 });
+
 
 /* References:
 For filtering functionality
